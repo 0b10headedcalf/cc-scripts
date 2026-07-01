ab7125c89dc6438c895541e62e085d6e3d747bb7
Admin <dc2000a@gmail.com>
2026-07-01 11:54:44 -0700

    update

--- notice.lua
+++ notice.lua
@@ -3,7 +3,6 @@ monitor.clear()
 
 local monmid = monitor.getSize()/2
 local lines = {}
-local xoffset = 0
 lines[1] = "Welcome to the Mob Grinder!"
 lines[2] = "Instructions:"
 lines[3] = "1. Right click the \"XP Holder\" block on the bottom"
@@ -11,14 +10,12 @@ lines[4] = "2. Shift+Right the Minus button to retrieve ~10 levels"
 lines[5] = "3. Ctrl+Right to retrieve all stored levels at once"
 lines[6] = "Happy Farming!"
 
-xoffset = string.len(lines[1])
-monitor.setCursorPos(monmid - xoffset,1)
+monitor.setCursorPos(monmid,1)
 monitor.write(lines[1])
 -- monitor.setTextScale(1)
 
 for i=2, #lines - 1 do
-    xoffset = string.len(lines[i])
-    monitor.setCursorPos(monmid - xoffset,i)
+    monitor.setCursorPos(1,i)
     monitor.write(lines[i])
 end
 
